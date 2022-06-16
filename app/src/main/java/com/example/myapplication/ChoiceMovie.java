a338eaf011b36706d80b6b97b4fe1254d89d3ffe
23qkd <23qkdxks@naver.com>
2022-06-16 16:20:48 +0900

    수정1 영화 포스터 추가 대화 상자 추가

--- app/src/main/java/com/example/myapplication/ChoiceMovie.java
+++ app/src/main/java/com/example/myapplication/ChoiceMovie.java
@@ -1,44 +1,97 @@
 package com.example.myapplication;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class ChoiceMovie extends MovieReview {
+public class ChoiceMovie extends AppCompatActivity {
 
-    ImageButton ib1, ib2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie);
 
-        ib1 = findViewById(R.id.ib1);
-        ib2 = findViewById(R.id.ib2);
+        final GridView gv = findViewById(R.id.gridmovie);
+        MyGridAdapter gAdapter = new MyGridAdapter(this);
+        gv.setAdapter(gAdapter);
+    }
 
-        ib1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    public class MyGridAdapter extends BaseAdapter{
+        Context context;
+        public MyGridAdapter(Context c){
+            context = c;
+        }
 
-                Intent intent = new Intent(getApplicationContext(),MovieReview.class);
-                startActivity(intent);
+        @Override
+        public int getCount() {
+            return posterID.length;
+        }
 
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
 
-            }
-        });
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
 
-        ib2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MovieReview.class);
-                startActivity(intent);
-            }
-        });
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ImageView iv = new ImageView(context);
+            iv.setLayoutParams(new ViewGroup.LayoutParams(1500,500));
+            iv.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            iv.setPadding(20,20,20,20);
+            iv.setImageResource(posterID[i]);
 
+            final int pos = i;
+            iv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    View dialogView = View.inflate(ChoiceMovie.this, R.layout.dialog, null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(ChoiceMovie.this);
+                    ImageView ivPoster = dialogView.findViewById(R.id.ivPoster);
+                    ivPoster.setImageResource(posterID[pos]);
+
+                    dlg.setTitle(posterName[pos]);
+                    dlg.setIcon(R.drawable.movie_icon);
+                    dlg.setView(dialogView);
+                    dlg.setNegativeButton("등록", null);
+                    dlg.show();
+                }
+            });
+            return iv;
+
+        }
+
+        Integer[] posterID = {
+                R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04,
+                R.drawable.mov05, R.drawable.mov06, R.drawable.mov07, R.drawable.mov08,
+                R.drawable.mov09, R.drawable.mov10, R.drawable.mov11, R.drawable.mov12,
+                R.drawable.mov13, R.drawable.mov14, R.drawable.mov15, R.drawable.mov16
+        };
+
+        String[] posterName = {
+                "토이스토리4", "호빗 다섯군대전투", "제이슨 본", "반지의 제왕", "정직한 후보",
+                "나쁜녀석들 포에버 ","겨울왕국2", "알라딘", "극한직업", "스파이더맨 파 프롬 홈",
+                "레옹","주먹왕 랄프2", "타짜:원 아이드 잭", "걸캅스", "도굴","어벤져스 앤드게임"
+        };
     }
 }
+
+
+
